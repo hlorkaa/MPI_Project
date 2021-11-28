40236753af457b5300c7f40af9d4d404d4bb2611
@penkovoy <makspen98@mail.ru>
2021-11-28 13:36:48 +0300

    fixed display of tasks on workman page

--- src/main/java/com/example/MPI_Project/controller/WorkmanController.java
+++ src/main/java/com/example/MPI_Project/controller/WorkmanController.java
@@ -1,14 +1,17 @@
 package com.example.MPI_Project.controller;
 
+import com.example.MPI_Project.config.SecurityHelper;
 import com.example.MPI_Project.domain.Task;
 import com.example.MPI_Project.repos.TaskRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 
 @Controller
@@ -16,10 +19,17 @@ import java.util.Map;
 public class WorkmanController {
     @Autowired
     private TaskRepo taskRepo;
+    private SecurityHelper secHelper;
 
-    public void putVariables(String workman_name, Map<String, Object> model, Integer id, String name, String deadline, String status, String description, String workman) {
+    @PostConstruct
+    public void initialize() {
+        secHelper = new SecurityHelper(SecurityContextHolder.getContext());
+    }
 
-        Iterable<Task> tasks = taskRepo.findByWorkman(workman_name);
+    public void putVariables(Map<String, Object> model, Integer id, String name, String deadline, String status, String description, String workman) {
+
+        String workmanName = secHelper.userName();
+        Iterable<Task> tasks = taskRepo.findByWorkman(workmanName);
         model.put("tasks", tasks);
         model.put("task_id", id);
         model.put("task_name", name);
@@ -38,7 +48,7 @@ public class WorkmanController {
             @RequestParam(name="workman_name", required=false, defaultValue="") String workman_name,
             Map<String, Object> model) {
 
-        putVariables(workman_name, model, 0,  "",  "",  "",  "",  "");
+        putVariables(model, 0,  "",  "",  "",  "",  "");
         return "workman_temp";
     }
 
@@ -55,7 +65,7 @@ public class WorkmanController {
         String task_description = task.getDescription();
         String task_workman = task.getWorkman();
 
-        putVariables(workman_name, model, chooseTask_id,  task_name,  task_deadline,  task_status,  task_description,  task_workman);
+        putVariables(model, chooseTask_id,  task_name,  task_deadline,  task_status,  task_description,  task_workman);
 
         return "workman_temp";
     }
@@ -81,7 +91,7 @@ public class WorkmanController {
             taskRepo.save(task);
         }
 
-        putVariables(workman_name, model, 0,  "",  "",  "",  "",  "");
+        putVariables(model, 0,  "",  "",  "",  "",  "");
 
         return "workman_temp";
     }
@@ -90,7 +100,7 @@ public class WorkmanController {
     public String cancelTaskEdition (
             @RequestParam(name="workman_name", required=false, defaultValue="") String workman_name,
             Map<String, Object> model) {
-        putVariables(workman_name, model, 0,  "",  "",  "",  "",  "");
+        putVariables(model, 0,  "",  "",  "",  "",  "");
 
         return "workman_temp";
     }
